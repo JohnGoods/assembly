953664b87cb3114845cf697cda865234d57edaed
JiangJianghao <469672930@qq.com>
2017-12-28 23:17:53 +0800

    找到函数call
    找到函数call

--- 汇编/汇编/汇编.cpp
+++ 汇编/汇编/汇编.cpp
@@ -2,15 +2,21 @@
 
 #include "stdafx.h"
 #include <Windows.h>
-int aaa;
+
+int add(int a,int b){
+	return a + b;
+}
+
+int bbb = 1;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	MessageBoxW(0, NULL, NULL, MB_OK);
-	aaa = 0x889977;	
+	bbb = 0x889977;	
 	//00E21016  |.  C705 6C33E200 77998800 
 	//mov dword ptr ds:[aaa],0x889977 
-	__asm MOV DWORD PTR DS : [aaa], 0x889977 //不安全的写法
-	__asm mov aaa, 0x889977;//安全的写法
+	__asm MOV DWORD PTR DS : [bbb], 0x889977 //不安全的写法
+	__asm mov bbb, 0x889977;//安全的写法
+	bbb = add(bbb, 0x88);
 	MessageBoxW(0, NULL, NULL, MB_OK);
 	return 0;
 }
