c37b8fb07e9888a5f94c635ef7bf567af21e3db3
JiangJianghao <469672930@qq.com>
2017-12-28 22:31:11 +0800

    汇编中安全与不安全的写法
    汇编中安全与不安全的写法

--- 汇编/汇编/汇编.cpp
+++ 汇编/汇编/汇编.cpp
@@ -9,6 +9,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	aaa = 0x889977;	
 	//00E21016  |.  C705 6C33E200 77998800 
 	//mov dword ptr ds:[aaa],0x889977 
+	__asm MOV DWORD PTR DS : [aaa], 0x889977 //不安全的写法
+	__asm mov aaa, 0x889977;//安全的写法
 	MessageBoxW(0, NULL, NULL, MB_OK);
 	return 0;
 }
