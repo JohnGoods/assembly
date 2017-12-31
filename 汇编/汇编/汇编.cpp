2ae07e72c32756f0eecb9a1bbcf5c668339fd528
JiangJianghao <469672930@qq.com>
2017-12-31 14:45:10 +0800

    MOV指令的寻址方式
    MOV指令的寻址方式

--- 汇编/汇编/汇编.cpp
+++ 汇编/汇编/汇编.cpp
@@ -8,6 +8,7 @@ int add(int a,int b){
 }
 
 int bbb = 1;
+int a[0x10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xd, 0xe, 0xF };//4字节
 int _tmain(int argc, _TCHAR* argv[])
 {
 	MessageBoxW(0, NULL, NULL, MB_OK);
@@ -17,6 +18,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	__asm MOV DWORD PTR DS : [bbb], 0x889977 //不安全的写法
 	__asm mov bbb, 0x889977;//安全的写法
 	bbb = add(bbb, 0x88);
+	//测试寻址代码
+	int *da = a;//
+	//提示 在命令栏中用 dd 地址表达式 可以以DWORD  4字节格式显示指定地址的数据
+	__asm
+	{
+		mov eax, 0x1111
+			mov ebx, eax
+			mov ebx, bbb //全局变量bbb
+			mov ebx, da //取数组基址
+			mov eax, [da + 4]//错的写法 编译不会出错
+			mov eax, [ebx + 4]
+	}
 	MessageBoxW(0, NULL, NULL, MB_OK);
 	return 0;
 }
