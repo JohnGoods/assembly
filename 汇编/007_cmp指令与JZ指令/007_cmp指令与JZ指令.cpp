c74915f24b5869acf12e34e40328fdf1af770dff
JiangJianghao <469672930@qq.com>
2018-01-02 22:24:22 +0800

    cmp指令与JZ指令
    cmp指令与JZ指令

---
+++ 汇编/007_cmp指令与JZ指令/007_cmp指令与JZ指令.cpp
@@ -0,0 +1,19 @@
+// 007_cmp指令与JZ指令.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("begin\n");
+	int a = 13;
+	if (a != 3) //== > >= < <=
+	{//a!=3
+		printf("do if");
+	}
+	//a==3
+	printf("end");
+	return 0;
+}
+
