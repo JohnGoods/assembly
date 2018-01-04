fe04161a4cfdb360b8964ed41076ab0444948789
JiangJianghao <469672930@qq.com>
2018-01-04 20:58:53 +0800

    无符号数条件转移指令JB JNAE JC(小于)
    无符号数条件转移指令JB JNAE JC(小于)

---
+++ 汇编/014_无符号数条件转移指令JB JNAE JC(小于)/014_无符号数条件转移指令JB JNAE JC(小于).cpp
@@ -0,0 +1,19 @@
+// 014_无符号数条件转移指令JB JNAE JC(小于).cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("begin\n");
+	int a = 3, b = -5;
+	unsigned int a2 = a, b2 = b;
+	if (a2 >= b2)//无符号的生成JAE JNB	 不够减借位
+	if (a >= b) ///<时不跳转>时跳转带符号的生成JGE JNL指令
+	{
+		printf("do if\n");
+	}
+	return 0;
+}
+
