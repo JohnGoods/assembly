7206c6120ed6fd627a64d21c2a85a8f396126bd8
JiangJianghao <469672930@qq.com>
2018-01-07 18:56:17 +0800

    浮点指令CVTTPS2PI(浮点数转整数)
    浮点指令CVTTPS2PI(浮点数转整数)

---
+++ 汇编/031_浮点指令CVTTPS2PI(浮点数转整数)/031_浮点指令CVTTPS2PI(浮点数转整数).cpp
@@ -0,0 +1,14 @@
+// 031_浮点指令CVTTPS2PI(浮点数转整数).cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	float f1 = 666.886;
+	int i = 2;
+	i = f1;
+	return 0;
+}
+
