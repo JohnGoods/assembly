c6402fc38845e4ca361db31ea5608d91e0c8056b
JiangJianghao <469672930@qq.com>
2018-01-07 13:08:38 +0800

    dec(减1)和inc(加1)指令
    dec(减1)和inc(加1)指令

---
+++ 汇编/024_dec(减1)和inc(加1)指令/024_dec(减1)和inc(加1)指令.cpp
@@ -0,0 +1,16 @@
+// 024_dec(减1)和inc(加1)指令.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+int i;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	i++;//inc
+	printf("%d", i);
+	i--;//dec
+	printf("%d", i);
+	return 0;
+}
+
