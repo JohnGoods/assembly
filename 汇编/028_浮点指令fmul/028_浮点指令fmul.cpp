f16e3066ce7e0a1890f431981b22d81393b73d47
JiangJianghao <469672930@qq.com>
2018-01-07 18:30:36 +0800

    浮点指令fmul
    浮点指令fmul

---
+++ 汇编/028_浮点指令fmul/028_浮点指令fmul.cpp
@@ -0,0 +1,17 @@
+// 028_浮点指令fmul.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("begin...");
+	double f1, f2;//float;
+	f1 = 3.333f;
+	f2 = 6.366f;
+	printf("f1...f2");
+	f1 = f1*f2;
+	return 0;
+}
+
