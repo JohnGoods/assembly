ea055e43ba50732747c274d635212702780478cc
JiangJianghao <469672930@qq.com>
2018-01-07 18:42:45 +0800

    浮点指令fmul
    浮点指令fmul

--- 汇编/028_浮点指令fmul/028_浮点指令fmul.cpp
+++ 汇编/028_浮点指令fmul/028_浮点指令fmul.cpp
@@ -6,12 +6,9 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("begin...");
-	double f1, f2;//float;
+	float f1, f2;//float;
 	f1 = 3.333f;
 	f2 = 6.366f;
-	printf("f1...f2");
 	f1 = f1*f2;
 	return 0;
 }
-
