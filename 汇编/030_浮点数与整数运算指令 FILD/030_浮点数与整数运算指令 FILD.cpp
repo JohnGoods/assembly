c6c52bdbf96815f67c5d34db179f7fecc21e061e
JiangJianghao <469672930@qq.com>
2018-01-07 18:52:39 +0800

    浮点数与整数运算指令
    浮点数与整数运算指令

---
+++ 汇编/030_浮点数与整数运算指令 FILD/030_浮点数与整数运算指令 FILD.cpp
@@ -0,0 +1,13 @@
+// 030_浮点数与整数运算指令 FILD.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	double f1 = 666.886;
+	int i = 2;
+	f1 = f1 + i;
+	return 0;
+}
