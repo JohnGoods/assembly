a66d671e1c2634c1bc6e0948fe4a12d1cc87f1d5
JiangJianghao <469672930@qq.com>
2018-01-07 18:42:21 +0800

    浮点指令FDIV(除)
    浮点指令FDIV(除)

---
+++ 汇编/029_浮点指令FDIV(除)/029_浮点指令FDIV(除).cpp
@@ -0,0 +1,14 @@
+// 029_浮点指令FDIV(除).cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	float f1, f2;
+	f1 = 3.33;
+	f2 = 2.00;
+	f1 = f1 / f2;
+	return 0;
+}
